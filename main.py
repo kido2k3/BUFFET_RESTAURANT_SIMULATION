899e94a00edfc4c0652d2d52d284b970244185ba
longngyuyen <93181708+longngyuyen@users.noreply.github.com>
2023-11-21 08:15:45 +0700

    Update main.py
    added max_queue_size for ticket queue
    added max_cus_in and clear the ticket queue when the restaurant is full

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import random
 import numpy
 import queue
 
-TIME_INTERVAL = [30/60, 100/60, 30/60, 15/60, 5/60]
+TIME_INTERVAL = [30/60, 100/60, 30/60, 15/60, 2/60]
 # --> the lambda of exponential distribution
 TIME_CAN_WAIT = 8/60
 RANDOM_SEED = 42
 RATE_TAKING_FOOD = [0.9, 0.8, 0.99, 0.85]
+MAX_QUEUE_SIZE = 5
+
 # random.seed(RANDOM_SEED)
 
 '''
@@ -36,7 +38,7 @@ class Customer:
         self.rating = 5
         self.left_time = 0
         self.taking_food = [1, 1, 1, 1]
-        self.doesnt_want_to_buy_food = 1
+        self.not_want_to_get_food = 1
 
     def classify(self):
         for i in range(4):
@@ -45,7 +47,7 @@ class Customer:
                 a=[0, 1], p=[1-rate, rate])
             # print(self.taking_food[i])
             if self.taking_food[i] == 1:
-                self.doesnt_want_to_buy_food = 0
+                self.not_want_to_get_food = 0
 
 
     def set_waiting_time(self, waiting_time):
@@ -53,11 +55,10 @@ class Customer:
         # update rating
         if waiting_time > TIME_CAN_WAIT:
             decrease = (self.waiting_time//TIME_CAN_WAIT)*0.5
-            print(f"Customer {self.id} waited for {self.waiting_time:7f} and decreased the rating by {decrease}")    
+            print(f"Customer {self.id} waited for {self.waiting_time:7f} and decreased the rating by {decrease}, ", end='')    
             self.rating = self.rating - decrease
-
-    def reset_waiting_time(self):
-        self.waiting_time = 0
+            return True
+        return False
 
     def pick_food_to_queue(self):
         indices_with_one = [index for index, value in enumerate(self.taking_food) if value == 1]
@@ -68,7 +69,7 @@ class Customer:
         return random_index
     
     def is_full_desired_dish(self):
-        if self.doesnt_want_to_buy_food == 1:
+        if self.not_want_to_get_food == 1:
             return 0
         for i in range(4):
             if self.taking_food[i] == 1:
@@ -76,7 +77,7 @@ class Customer:
         return 1
     
     def is_not_want_to_eat(self):
-        return self.doesnt_want_to_buy_food
+        return self.not_want_to_get_food
 
 
 '''
@@ -92,38 +93,97 @@ brief:
 
 class Restaurant:
     def __init__(self, env):
-        self.NUMBER_OF_TICKET_SERVERS = 2
-        self.TIME_TICKET_SERVICE = 5
-        self.NUMBER_OF_DRINKS_SERVICE = 2
-        self.TIME_DRINKS_SERVICE = 5
-        self.NUMBER_OF_APPETIZER_SERVICE = 2
-        self.TIME_OF_APPETIZER_SERVICE = 5
-        self.NUMBER_OF_MAINCOURSE_SERVICE = 2
-        self.TIME_OF_MAINCOURSE_SERVICE = 5
-        self.NUMBER_OF_DESSERTS_SERVICE = 2    
-        self.TIME_DESSERTS_SERVICE = 5
+        self.NUMBER_OF_TICKET_SERVERS = 1
+        self.TIME_TICKET_SERVICE = 1
+        self.NUMBER_OF_DRINKS_SERVICE = 10
+        self.TIME_DRINKS_SERVICE = 10
+        self.NUMBER_OF_APPETIZER_SERVICE = 10
+        self.TIME_OF_APPETIZER_SERVICE = 10
+        self.NUMBER_OF_MAINCOURSE_SERVICE = 10
+        self.TIME_OF_MAINCOURSE_SERVICE = 10
+        self.NUMBER_OF_DESSERTS_SERVICE = 10 
+        self.TIME_DESSERTS_SERVICE = 10
         self.TIME_SEAT = 2
-        self.NUMBER_OF_SEATS= 99
+        self.NUMBER_OF_SEATS= 50
+
+        self.ticket_queue_size = 0
+        self.num_cus_in = 0
+        self.num_cus_wait_long = 0
+        self.num_Cus_leave_ticketFull = 0
+        self.num_Cus_leave_resFull = 0
+
         self.server_ticket = simpy.Resource(env, self.NUMBER_OF_TICKET_SERVERS)
         self.server_mainCourses = simpy.Resource(env, self.NUMBER_OF_MAINCOURSE_SERVICE)
         self.server_appetizer = simpy.Resource(env, self.NUMBER_OF_APPETIZER_SERVICE)
         self.server_drinks = simpy.Resource(env, self.NUMBER_OF_DRINKS_SERVICE)
         self.server_desserts = simpy.Resource(env, self.NUMBER_OF_DESSERTS_SERVICE)
         self.server_Seat_eat = simpy.Resource(env, self.NUMBER_OF_SEATS)
-        self.choosing_food_queue =  queue.Queue()
+        self.event_full = simpy.Event(env)
+        
+        self.num_use_ticket = 0
+        self.num_use_mainCourses = 0
+        self.num_use_appetizer = 0
+        self.num_use_drinks = 0
+        self.num_use_desserts = 0
+        self.num_rated = 0
+        self.sum_serve_time_ticket  = 0
+        self.sum_serve_time_mainCourses = 0
+        self.sum_serve_time_appetizer = 0
+        self.sum_serve_time_drinks = 0
+        self.sum_serve_time_desserts = 0 
+        self.num_ticket_wait_long = 0
+        self.num_drinks_wait_long = 0
+        self.num_appetizer_wait_long = 0
+        self.num_mainCourses_wait_long = 0
+        self.num_desserts_wait_long = 0
+        self.sum_rated = 0
+        self.sum_rated_no_neg = 0
 
     def serve_ticket(self, env, consumer, arrival_time):
+        consumer.arrival_time = arrival_time
+        if self.ticket_queue_size >= MAX_QUEUE_SIZE:
+                self.num_Cus_leave_ticketFull += 1
+                print(f"Ticket queue is full, Customer {consumer.id} left")
+                return
+        else:
+            self.ticket_queue_size+=1
+        
+        local_event_full = self.event_full
+        #entering_Ticket_queue
         with self.server_ticket.request() as req:
-            yield req
-            consumer.arrival_time = arrival_time
-            print(f'Customer {consumer.id:3} entered ticket at {env.now:7.3f}')
+            yield req | local_event_full
+            if local_event_full.triggered:
+                print(f"The restaurant is full so customer {consumer.id} left the ticket queue #####################################################################")
+                self.num_Cus_leave_resFull += 1
+                self.ticket_queue_size = 0
+                return
+    
+            print(f'Customer {consumer.id:3} was getting serve at ticket at {env.now:7.3f}')
             serve_time = random.expovariate(self.TIME_TICKET_SERVICE)
-            yield env.timeout(serve_time)
-            consumer.set_waiting_time(env.now - consumer.arrival_time)
-            print(f'Customer {consumer.id:3} left ticket at {env.now:7.3f}')
+            yield env.timeout(serve_time) | local_event_full
+                
+            if local_event_full.triggered:
+                print(f"The restaurant is full so customer {consumer.id} has left the ticket queue #####################################################################")
+                self.num_Cus_leave_resFull += 1
+                self.ticket_queue_size = 0
+                return
+
+
+            self.ticket_queue_size -=1
+            self.num_cus_in += 1
+            if (self.num_cus_in >= self.NUMBER_OF_SEATS):
+                self.event_full.succeed()
+
+            if consumer.set_waiting_time(env.now - consumer.arrival_time):
+                self.num_cus_wait_long+=1
+                self.num_ticket_wait_long+=1
+           
+            self.num_use_ticket += 1
+            self.sum_serve_time_ticket += serve_time
+            print(f'Customer {consumer.id:3} left ticket at {env.now:7.3f}, the restaurant has {self.num_cus_in} customers')
             self.choose_food(env,consumer)
      
-    def choose_food(self,env,customer ):
+    def choose_food(self,env,customer):
         chosen_food = customer.pick_food_to_queue()      
         if chosen_food == 0:
             env.process(self.serve_drinks(env,customer))
@@ -135,59 +195,87 @@ class Restaurant:
             env.process(self.serve_desserts(env,customer))
         elif customer.is_full_desired_dish():
             env.process(self.serve_seat_eat(env,customer))
-        elif customer.doesnt_want_to_eat():
+        elif customer.is_not_want_to_eat():
             self.leave_n_rate(env,customer)
         else:
             print("Something went wrong!")
 
     def serve_drinks(self, env, consumer):
-        with  self.server_drinks.request() as req:
+        consumer.arrival_time = env.now
+        print(f'Customer {consumer.id:3} entered drinks queue at {env.now:7.3f}')
+        with self.server_drinks.request() as req:
             yield req
-            consumer.arrival_time = env.now
-            print(f'Customer {consumer.id:3} entered drinks at {env.now:7.3f}')
+            print(f'Customer {consumer.id:3} was getting served at drinks at {env.now:7.3f}')
             serve_time = random.expovariate(self.TIME_DRINKS_SERVICE)
             yield env.timeout(serve_time)
-            consumer.set_waiting_time(env.now - consumer.arrival_time)
-            self.choosing_food_queue.put(consumer)
+            
+            self.num_use_drinks += 1
+            self.sum_serve_time_drinks += serve_time
+
+            if consumer.set_waiting_time(env.now - consumer.arrival_time):
+                self.num_cus_wait_long+=1
+                self.num_drinks_wait_long+=1
+
             consumer.taking_food[0] = 0
             print(f'Customer {consumer.id:3} left drinks at {env.now:7.3f}')
             self.choose_food(env,consumer)
     
     def serve_appetizer(self, env, consumer):
+        consumer.arrival_time = env.now
+        print(f'Customer {consumer.id:3} entered appetizer queue at {env.now:7.3f}')
         with  self.server_appetizer.request() as req:
             yield req
-            consumer.arrival_time = env.now
-            print(f'Customer {consumer.id:3} entered appetizer at {env.now:7.3f}')
+            print(f'Customer {consumer.id:3} was getting served at appetizer at {env.now:7.3f}')
             serve_time = random.expovariate(self.TIME_OF_APPETIZER_SERVICE)
             yield env.timeout(serve_time)
-            consumer.set_waiting_time(env.now - consumer.arrival_time)
-            self.choosing_food_queue.put(consumer)
+
+            self.num_use_appetizer += 1
+            self.sum_serve_time_appetizer += serve_time
+
+            if consumer.set_waiting_time(env.now - consumer.arrival_time):
+                self.num_cus_wait_long+=1
+                self.num_appetizer_wait_long+=1
+
             consumer.taking_food[1] = 0
             print(f'Customer {consumer.id:3} left appetizer at {env.now:7.3f}')
             self.choose_food(env,consumer)
 
     def serve_mainCourses(self, env, consumer):
+        consumer.arrival_time = env.now
+        print(f'Customer {consumer.id:3} entered mainCourse queue at {env.now:7.3f}')
         with  self.server_mainCourses.request() as req:
             yield req
-            consumer.arrival_time = env.now
-            print(f'Customer {consumer.id:3} entered mainCourse at {env.now:7.3f}')
+            print(f'Customer {consumer.id:3} was getting served at mainCourse at {env.now:7.3f}')
             serve_time = random.expovariate(self.TIME_OF_MAINCOURSE_SERVICE)
             yield env.timeout(serve_time)
-            consumer.set_waiting_time(env.now - consumer.arrival_time)
-            self.choosing_food_queue.put(consumer)
+
+            self.num_use_mainCourses += 1
+            self.sum_serve_time_mainCourses += serve_time
+
+            if consumer.set_waiting_time(env.now - consumer.arrival_time):
+                self.num_cus_wait_long+=1
+                self.num_mainCourses_wait_long+=1
+
             consumer.taking_food[2] = 0
             print(f'Customer {consumer.id:3} left mainCourse at {env.now:7.3f}')
             self.choose_food(env,consumer)
     
     def serve_desserts(self, env, consumer):
+        consumer.arrival_time = env.now
+        print(f'Customer {consumer.id:3} entered desserts queue at {env.now:7.3f}')
         with  self.server_desserts.request() as req:
             yield req
-            consumer.arrival_time = env.now
-            print(f'Customer {consumer.id:3} entered desserts at {env.now:7.3f}')
+            print(f'Customer {consumer.id:3} was getting served at desserts at {env.now:7.3f}')
             serve_time = random.expovariate(self.TIME_DESSERTS_SERVICE)
             yield env.timeout(serve_time)
-            consumer.set_waiting_time(env.now - consumer.arrival_time)
-            self.choosing_food_queue.put(consumer)
+
+            self.num_use_desserts += 1
+            self.sum_serve_time_desserts += serve_time
+
+            if consumer.set_waiting_time(env.now - consumer.arrival_time):
+                self.num_cus_wait_long+=1
+                self.num_desserts_wait_long+=1
+
             consumer.taking_food[3] = 0
             print(f'Customer {consumer.id:3} left desserts at {env.now:7.3f}')
             self.choose_food(env,consumer)
@@ -195,27 +283,34 @@ class Restaurant:
     def serve_seat_eat(self,env,consumer):
         with self.server_Seat_eat.request() as req:
             yield req
-            #consumer.set_waiting_time(env.now - consumer.arrival_time)
-            print(f'Customer {consumer.id:3} entered seat to eat at {env.now:7.3f}')
+            print(f'Customer {consumer.id:3} entered a seat to eat at {env.now:7.3f}')
             serve_time = random.expovariate(self.TIME_SEAT)
             yield env.timeout(serve_time)
             print(f'Customer {consumer.id:3} has finished their dinner at {env.now:7.3f}')
             self.leave_n_rate(env,consumer)
 
     def leave_n_rate(self,env,consumer):
-        print(f"Customer {consumer.id:3} left the restaurant at {env.now:7.3f} and rated {consumer.rating} stars")
+        self.num_cus_in -= 1
+        self.num_rated +=1
+        self.sum_rated += consumer.rating
+        if consumer.rating > 0:
+            self.sum_rated_no_neg += consumer.rating
+        if self.event_full.triggered:
+            print(f"So the restaurant currently have {self.num_cus_in} customers")
+            self.event_full = simpy.Event(env)
+        print(f"Customer {consumer.id:3} left the restaurant at {env.now:7.3f} and rated {consumer.rating} stars, the restaurant has {self.num_cus_in} customers")
+        
 
 
-def generate(env, customers, TIME_INTERVAL, server_generator, restaurant):
+def generate(env, TIME_INTERVAL, server_generator, restaurant):
     with server_generator.request() as req:
         yield req
         i = 0
         while True:
             consumer = Customer(i, arrival_time=env.now)
             consumer.classify()
-            customers.append(consumer)
             print(
-                f'Customer {consumer.id:3} arrived at {consumer.arrival_time:7.3f}')
+                f'Customer {consumer.id:3} arrived at {consumer.arrival_time:7.3f} and entered ticket queue (cus_in:{restaurant.num_cus_in})')
             env.process(restaurant.serve_ticket(
                 env, consumer, consumer.arrival_time))
             if env.now < 60:  # 5h-6h
@@ -236,11 +331,20 @@ def generate(env, customers, TIME_INTERVAL, server_generator, restaurant):
 
 
 env = simpy.Environment()
-customers = list(())
 restaurant = Restaurant(env)
 server_generator = simpy.Resource(env, 1)
-
-env.process(generate(env, customers, TIME_INTERVAL,
+env.process(generate(env, TIME_INTERVAL,
             server_generator, restaurant))
 
 env.run(until=60*5)
+
+print(f'Time can wait: {TIME_CAN_WAIT}')
+print(f'Ticket -  Uses: {restaurant.num_use_ticket}     AverageServe: {restaurant.sum_serve_time_ticket/restaurant.num_use_ticket:7.3f}     WaitLong: {restaurant.num_ticket_wait_long}')
+print(f'Drinks -  Uses: {restaurant.num_use_drinks}     AverageServe: {restaurant.sum_serve_time_drinks/restaurant.num_use_drinks:7.3f}     WaitLong: {restaurant.num_drinks_wait_long}')
+print(f'Appetizer -  Uses: {restaurant.num_use_appetizer}     AverageServe: {restaurant.sum_serve_time_appetizer/restaurant.num_use_appetizer:7.3f}     WaitLong: {restaurant.num_appetizer_wait_long}')
+print(f'mainCourses -  Uses: {restaurant.num_use_mainCourses}     AverageServe: {restaurant.sum_serve_time_mainCourses/restaurant.num_use_mainCourses:7.3f}     WaitLong: {restaurant.num_mainCourses_wait_long}')
+print(f'Desserts -  Uses: {restaurant.num_use_desserts}     AverageServe: {restaurant.sum_serve_time_desserts/restaurant.num_use_desserts:7.3f}     WaitLong: {restaurant.num_desserts_wait_long}')
+print(f'Number of rating: {restaurant.num_rated}    AverageRating:{restaurant.sum_rated/restaurant.num_rated}   AverageRating_MinZero:{restaurant.sum_rated_no_neg/restaurant.num_rated}')
+print(f'There were {restaurant.num_Cus_leave_ticketFull} times that customers left the restaurant because the ticket queue was full')
+print(f'There were {restaurant.num_Cus_leave_resFull} times that customers left the restaurant because the restaurant was full')
+print(f'There were {restaurant.num_cus_wait_long} times that customers had to wait too long ({restaurant.num_ticket_wait_long + restaurant.num_drinks_wait_long + restaurant.num_appetizer_wait_long + restaurant.num_mainCourses_wait_long + restaurant.num_desserts_wait_long})')
